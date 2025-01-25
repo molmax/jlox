bb81c868ae9361fdeb0acb6f02e5ebc102dc19b0
molmax <gr33nengineer@gmail.com>
2025-01-25 11:42:44 +0300

    Add right brace token type

--- lox/TokenType.java
+++ lox/TokenType.java
@@ -2,8 +2,8 @@ package lox;
 
 enum TokenType {
     //Single-character tokens
-    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, COMMA,
-    DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
     //One or two character tokens
     BANG, BANG_EQUAL, EQUAL_EQUAL,GREATER,
